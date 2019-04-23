5b58eed9a0954169e893e0316dffcc3d953a1cce
Bruno Gouvêa Roldão <Brunogrbhg@gmail.com>
2019-04-23 13:51:49 -0300

    implementando template

---
+++ Source/Domain/BuscarFilmesCoreException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// TODO: REMOVER A PASTA "DOMAIN" E TODOS OS SEUS ARQUIVOS
+/// Classe mockada apenas para referência do projeto.
+/// </summary>
+namespace ApiAdapter.Template.Domain
+{
+    public class BuscarFilmesCoreException : Exception
+    {
+        public BuscarFilmesCoreException(BuscarFilmesCoreError buscarFilmesCoreError)
+        {
+
+        }
+    }
+
+    public class BuscarFilmesCoreError
+    {
+        public static BuscarFilmesCoreError LimiteDeRequisicoesAtingido =>
+           new BuscarFilmesCoreError("LimiteDeRequisicoesAtingido", "O limite de requisições ao provedor de filmes foi atingido, tente novamente mais tarde.");
+
+        protected BuscarFilmesCoreError(string key, string message)
+        {
+        }
+    }
+}
---
+++ Source/Domain/ITmdbAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// TODO: REMOVER A PASTA "DOMAIN" E TODOS OS SEUS ARQUIVOS
+/// Classe mockada apenas para referência do projeto.
+/// </summary>
+namespace ApiAdapter.Template.Domain
+{
+    public interface ITmdbAdapter
+    {
+        Task<IEnumerable<Filme>> GetFilmesAsync(Pesquisa pesquisa, string idioma);
+    }
+}
